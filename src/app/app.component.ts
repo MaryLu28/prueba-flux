9814cbf22019bd27fc25863ce19d228c45c4a573
María Lourdes <marylo281292@gmail.com>
2020-09-29 01:50:40 -0300

    country added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,9 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 interface CurrentWeather {
+  request: {
+    query: string;
+  };
   current: {
     temperature: number;
   };
