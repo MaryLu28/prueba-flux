0c580a37b8ddd71b30388588acdba0a2f7706f30
María Lourdes <marylo281292@gmail.com>
2020-09-29 00:44:13 -0300

    functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+interface CurrentWeather {
+  current: {
+    temperature: number;
+  };
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'prueba-flux';
+  selected?: string;
+  newCity?: string;
+  weather$?: Observable<CurrentWeather>;
+  cities = ['Caracas', 'Santiago', 'Buenos Aires', 'New York'];
+
+  constructor(private readonly http: HttpClient) {}
+
+  onCityChange(city: string) {
+    this.selected = city;
+    this.weather$ = this.getWeather(city);
+  }
+
+  onAddCity() {
+    this.cities.push(this.newCity);
+  }
+
+  getWeather(city: string) {
+    const base = 'http://api.weatherstack.com/current';
+    const accessKey = '5d202b2713366934d49426cbad278071';
+    const url = `${base}?access_key=${accessKey}&query=${city}&units=m`;
+
+    return this.http.get<CurrentWeather>(url);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatSelectModule } from '@angular/material/select';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+// Material Modules
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatSelectModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
